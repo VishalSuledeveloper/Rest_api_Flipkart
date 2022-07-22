c5774ff49a29e113bfa8d8e614a32f30c1f480d0
Vishal <vssule@mitaoe.ac.in>
2022-07-22 19:45:57 +0530

    code

--- App.js
+++ App.js
@@ -19,14 +19,75 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send('Hiii From Express')
 })
-
 //products
 app.get('/product',(req,res) => {
-    db.collection('product').find().toArray((err,result) => {
+    let query={}
+    let brandID=Number(req.params.brandID);
+    let category_id =Number(req.params.category_id);
+    if(brandID){
+        query ={category_id:brandId}
+    }else if(category_id){
+        query={"Product_Type.category_id":category_id}
+    }
+    else{
+        query={}
+    }
+    db.collection('product').find(query).toArray((err,result) => {
         if(err) throw err;
         res.send(result)
     })
 })
+
+
+app.get('/filter/:costID',(req,res)=>{
+    let query={}
+    let sort = {cost:1}
+    let costID =Number(req.params.costID);
+    let brandID =Number(req.params.brandID);
+    let lcost=Number(req.params.lcost);
+    let hcost =Number(req.params.hcost);
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+    if(brandID && lcost && hcost){
+        query={
+            "cost.costID":costID,
+            "brand.brandID":brandID,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+
+    else if(brandID){
+        query={
+            "cost.costID":costID,
+            "brand.brandID":brandID
+        }
+    }else if(lcost && hcost){
+        query={
+            "cost.costID":costID,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+
+    }
+    else{
+        query={
+            "cost.costID":costID,
+        }
+    }
+    db.collection('product').find(query).sort(sort).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+
+})
+
+// //products
+// app.get('/product',(req,res) => {
+//     db.collection('product').find().toArray((err,result) => {
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 //brand
 app.get('/brand',(req,res) => {
     let id =req.query.category_id
@@ -43,6 +104,97 @@ app.get('/ProductType',(req,res) => {
         res.send(result)
     })
 })
+
+//details of the products
+app.get('/details/:id',(req,res) => {
+    let id = Number(req.params.id)
+    db.collection('product').find({category_id:id}).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+app.get('/PType/:id',(req,res) => {
+    let id = Number(req.params.id)
+    db.collection('Product_Type').find({category_id:id}).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+
+
+app.post('/productslist',(req,res) => {
+    if(Array.isArray(req.body.id)){
+        db.collection('product').find({category_id:{$in:req.body.id}}).toArray((err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+    }else{
+        res.send('Invalid Input')
+    }
+})
+
+
+
+//Place Order 
+app.post('/placeOrder',(req, res) =>{
+    db.collection('orders').insert(req.body,(err,result) =>{
+        if (err) throw err;
+        res.send ("order Placed")
+    })
+})
+
+//List of order Placed
+app.get('/orders',(req, res) =>{
+    let email =req.body.email;
+    let query ={}
+    if(email){
+        query={email}
+    }
+    db.collection('orders').find(query).toArray((err,result) =>{
+        if (err) throw err;
+        res.send(result)
+    })
+})
+
+app.put('/updateOrder/:id',(req,res)=>{
+    let oid=Number(req.params.id);
+    db.collection('orders').updateOne(
+        {orderId:oid},
+        {
+            $set:{
+                "status":req.body.status,
+                "bank_name":req.body.bank_name,
+                "date":req.body.date,
+            }
+        },(err,result) =>{
+            if (err) throw err;
+            res.send("order Updated")
+        }
+    )
+})
+
+
+//Delete order
+app.delete('/deleteOrder/:id',(req,res)=>{
+    let _id =mongo.ObjectId(req.params.id);
+    db.collection('orders').remove({_id},(err,result) =>{
+        if (err) throw err;
+        res.send("order Deleted")
+
+    })
+})
+// app.get('/details/:id',(req,res) => {
+//     let id = mongo.ObjectId(req.params.id)
+//     db.collection('product').find({_id:id}).toArray((err,result) => {
+//         if(err) throw err;
+//         res.send(result)
+//     })
+
+// })
 
 
 // app.get('/brand/:id',(req,res) => {
--- apiDocument.md
+++ apiDocument.md
@@ -14,4 +14,61 @@ http://localhost:9870/ProductType
 
 
 //Page2
+#product wrt brand
+http://localhost:9870/product?brandID=2
 
+#product wrt cost and brand
+http://localhost:9870/filter/5?brandID=5
+http://localhost:9870/filter/4?lcost=500&hcost=20000&brandID=4
+
+#Sort on basis of cost
+http://localhost:9870/filter/1?brandID=1&sort=-1
+
+//Page 3
+
+#details of the product
+http://localhost:9870/details/62da70ac7627463d23f62907
+http://localhost:9870/details/1
+
+//Products on the basis of product_type
+http://localhost:9870/PType/1
+
+//Page 4
+#Product Details
+(POST)localhost:9870/productslist
+
+{"id":[1,2,3]}
+
+#place order 
+(POST) localhost:9870/placeOrder
+    {
+        "orderId":5,
+        "name": "Ashvini",
+        "email": "ashvini@gmail.com",
+        "address": "Hno 24,Sector 1",
+        "phone": 922888082,
+        "cost": 18000,
+        "product":"Redmi Mobile",
+        "status": "delivered"
+    }
+
+//Page5
+#List of order Placed
+(GET)https://localhost:9870/orders
+
+#List of order place wrt email
+https://localhost:9870/orders?email=ashvini@gmail.com
+
+#update order details with payment 
+(PUT) https://localhost:9870/updateOrder/2
+
+{
+                "status":"TXN_SUCCESS",
+                "bank_name":"HDFC",
+                "date":"12/05/2022"
+}
+
+
+
+#Delete Order
+https://localhost:9870/deleteOrder/62daa3efad1beceec36ca360
